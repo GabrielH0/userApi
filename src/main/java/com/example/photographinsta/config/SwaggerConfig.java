97286b1a4dc29b5b8670c9f80f0e6ee3537fba3e
GabrielH0 <gabrielghb10@hotmail.com>
2022-03-04 16:43:16 -0300

    [003] - Swagger documentation

---
+++ src/main/java/com/example/photographinsta/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.example.photographinsta.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    public ApiInfo apiInfo(){
+        return new ApiInfoBuilder().title("User Rest API")
+                .description("User Management API")
+                .contact(new Contact("Gabriel Borges", "github.com/GabrielH0", "gabrielghb10@hotmail.com"))
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("0.0.1-SNAPSHOT")
+                .build();
+    }
+}
--- src/main/java/com/example/photographinsta/outbound/controller/UserApiController.java
+++ src/main/java/com/example/photographinsta/outbound/controller/UserApiController.java
@@ -3,6 +3,10 @@ package com.example.photographinsta.outbound.controller;
 import com.example.photographinsta.adapter.controller.UserBaseController;
 import com.example.photographinsta.adapter.model.UserRepresentation;
 import com.example.photographinsta.exception.AlreadyExistsException;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/user")
+@Api( value = "User management resource", description = "User management")
 public class UserApiController {
 
     private UserBaseController userBaseController;
@@ -19,6 +24,13 @@ public class UserApiController {
         this.userBaseController = userBaseController;
     }
 
+    @ApiOperation("Creates a new user")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(code = 200, message = "Sucessfull operation"),
+                    @ApiResponse(code = 400, message = "something went wrong in the operation")
+            }
+    )
     @PostMapping
     public ResponseEntity<UserRepresentation> newUser(@RequestBody UserRepresentation userRepresentation) {
         try {
@@ -29,6 +41,13 @@ public class UserApiController {
         }
     }
 
+    @ApiOperation("Search a user with the username passed")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(code = 200, message = "Sucessfull operation"),
+                    @ApiResponse(code = 404, message = "Username not found")
+            }
+    )
     @GetMapping("/{username}")
     public ResponseEntity<UserRepresentation> findByUsername(@PathVariable("username") String username) {
         UserRepresentation userRepresentationResponse = userBaseController.findByUsername(username);
