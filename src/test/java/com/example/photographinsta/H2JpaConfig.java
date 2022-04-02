090281782a029245092ef4a4dd1321c677e94fa6
GabrielH0 <gabrielghb10@hotmail.com>
2022-04-01 22:05:37 -0300

    [009]-configuration

--- src/test/java/com/example/photographinsta/H2JpaConfig.java
+++ src/test/java/com/example/photographinsta/H2JpaConfig.java
@@ -1,4 +1,4 @@
-package com.example.photographinsta.config;
+package com.example.photographinsta;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -7,31 +7,26 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 import java.util.Objects;
-import java.util.Properties;
 
+@Profile("test")
 @Configuration
 @EnableJpaRepositories(basePackages = "com.example.photographinsta.outbound.database.repository")
-@EnableTransactionManagement
-@Profile("Default")
-public class JpaConfig {
+public class H2JpaConfig {
 
     @Autowired
     private Environment env;
 
     @Bean
+    @Profile("test")
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName(Objects.requireNonNull(env.getProperty("spring.datasource.driver-class-name")));
         dataSource.setUrl(env.getProperty("spring.datasource.url"));
         dataSource.setUsername(env.getProperty("spring.datasource.username"));
         dataSource.setPassword(env.getProperty("spring.datasource.password"));
-        Properties properties = new Properties();
-        properties.setProperty("spring.jpa.generate-ddl", env.getProperty("spring.jpa.generate-ddl"));
-        dataSource.setConnectionProperties(properties);
         return dataSource;
     }
 }
--- src/test/java/com/example/photographinsta/PhotographInstaApplicationTests.java
+++ src/test/java/com/example/photographinsta/PhotographInstaApplicationTests.java
@@ -1,10 +1,14 @@
 package com.example.photographinsta;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {PhotographInstaApplication.class, H2JpaConfig.class})
+@ActiveProfiles("test")
 class PhotographInstaApplicationTests {
 
     @Test
